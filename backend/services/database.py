bc55074b5b3005b4b4baf946350bb92f74d26d72
Sash-J <sashenmjofficial@gmail.com>
2026-03-30 03:24:34 +0530

    versel bug fix 4

--- backend/services/database.py
+++ backend/services/database.py
@@ -8,10 +8,10 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", ""),
-        database=os.getenv("DB_NAME", "productions"),
+        host=os.getenv("DB_HOST", "sql12.freesqldatabase.com"),
+        user=os.getenv("DB_USER", "sql12821656"),
+        password=os.getenv("DB_PASSWORD", "PIRddhZMKg"),
+        database=os.getenv("DB_NAME", "sql12821656"),
         port=int(os.getenv("DB_PORT", 3306)),
         connection_timeout=5
     )
